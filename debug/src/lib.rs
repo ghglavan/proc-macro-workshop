2a5b284d97f95cbde9f130fb713b54c3a610ecd4
ghglavan <george.glavan27@gmail.com>
2021-05-03 22:15:13 +0300

    test 03

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,14 +1,51 @@
 use proc_macro::TokenStream;
-use quote::quote;
+use quote::{quote, ToTokens};
 
-use syn::{self, parse_macro_input, Data, DataStruct, DeriveInput};
+use proc_macro2;
+use syn;
 
-#[proc_macro_derive(CustomDebug)]
+struct DebugField {
+    ident: syn::Ident,
+    custom_fmt: Option<syn::Lit>,
+}
+
+impl DebugField {
+    fn parse(field: &syn::Field) -> Result<Self, TokenStream> {
+        let ident = field.ident.clone().unwrap();
+
+        let mut custom_fmt = None;
+
+        for attr in field.attrs.iter() {
+            match attr.parse_meta() {
+                Ok(syn::Meta::NameValue(nv)) => {
+                    if nv
+                        .path
+                        .is_ident(&syn::Ident::new("debug", proc_macro2::Span::call_site()))
+                    {
+                        custom_fmt = Some(nv.lit.clone());
+                    }
+                }
+                Ok(_) => {
+                    return Err(syn::Error::new_spanned(attr, "expected attr as ident")
+                        .into_compile_error()
+                        .into());
+                }
+                Err(e) => {
+                    return Err(e.into_compile_error().into());
+                }
+            }
+        }
+
+        Ok(Self { ident, custom_fmt })
+    }
+}
+
+#[proc_macro_derive(CustomDebug, attributes(debug))]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let input = parse_macro_input!(input as DeriveInput);
+    let input = syn::parse_macro_input!(input as syn::DeriveInput);
 
-    let idents = match input.data {
-        Data::Struct(DataStruct { fields, .. }) => fields.into_iter().map(|f| f.ident.unwrap()),
+    let fields = match input.data {
+        syn::Data::Struct(syn::DataStruct { fields, .. }) => fields,
         _ => {
             return syn::Error::new_spanned(input.ident, "we only know structs")
                 .into_compile_error()
@@ -16,9 +53,22 @@ pub fn derive(input: TokenStream) -> TokenStream {
         }
     };
 
+    let idents = fields.iter().map(|f| DebugField::parse(f).unwrap());
+
     let fields = idents.map(|f| {
-        quote! {
-            .field(stringify!(#f), &self.#f)
+        let ident = f.ident.clone();
+
+        match f.custom_fmt {
+            Some(l) => {
+                quote! {
+                    .field(stringify!(#ident), &::std::format_args!(#l , &self.#ident))
+                }
+            }
+            None => {
+                quote! {
+                    .field(stringify!(#ident), &self.#ident)
+                }
+            }
         }
     });
 
